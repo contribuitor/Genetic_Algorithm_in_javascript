d98a29548ba5d560be2d26eb6784cc7dcdc7acc1
钱利江 <1912227926@qq.com>
2014-08-26 14:23:03 +0800

    修改fitness，修改选取基因的方式，但是还是有问题
    （原来同一条基因可以被多次选中，现在一条只能选一次）
    发现基因的杂交，突变不能优化基因库，不知道是什么原因，中间有bug。。。
    实验发现：只在第0 generation找到就找到了，后面的generation 中再也不找不到

--- parseInt.js
+++ parseInt.js
@@ -2,8 +2,8 @@ POOL_SIZE = 40;
 GENE_LEN = 5;
 CROSSRATE = 0.7;
 MUTARATE = 0.001;
-
-TARGET = 21; //待分解
+var TARGET = document.getElementById("myNumber").value;
+Generation = 0;
 
 var genes = new Array;
 var newGenes = new Array;
@@ -14,23 +14,30 @@ var geneTable = ['0','1','2','3','4','5','6','7','8','9','+','-','*','/'];
 function doIt(){
 	var towGenes = new Array;
 	initGenePools();
+	//Generation = 0;
 	//alert(typeof(genes[1]));
-	for(var i=0; i<1000; i++){
-		twoGenes = selectMember(genes);
-		//alert(twoGenes);
-		twoGenes = crossover(twoGenes);//alert(twoGenes);
+	for(var i=0; i<10000; i++){
+		twoGenes = [selectMember(genes), selectMember(genes)];
+		//alert(twoGenes + ": genes before corssover");
+		twoGenes = crossover(twoGenes[0],twoGenes[1]);//alert(twoGenes);
+		//alert(twoGenes + ": genes after corssover");
 		twoGenes = mutate(twoGenes);
+		//alert(twoGenes + ": genes after mutate");
 		fitness(decode(twoGenes[0]));
 		fitness(decode(twoGenes[1]));
 		newGenes.push(twoGenes[0]);
 		newGenes.push(twoGenes[1]);
 		if(newGenes.length >= POOL_SIZE){
 			genes = newGenes;
-			newGenes = [];			
-			document.getElementById("info").innerHTML += "generation + 1";
+			newGenes = [];	
+			Generation += 1;
+			document.getElementById("info").innerHTML += Generation + " ";
+			//alert(genes);
 		}
 	}
 }
+
+//alert(decode("01100001101010000011"));
 
 /*****************************工具函数****************************/
 //初始化基因库
@@ -41,6 +48,7 @@ function initGenePools() {
 		for(var j=0; j<GENE_LEN*4; j++){
 			genes[i] += Math.round(Math.random()).toString();
 		}
+		
 	}
 	//newGenes = genes;
 	//alert(parseInt(genes[1], 2));
@@ -83,66 +91,66 @@ function fitness(expression){
 	}
 	//document.getElementById("info").innerHTML += "generation + 1; ";
 	//alert("the value of the expression is: " + tot);
-	if(TARGET - tot == 0) {document.getElementById("info").innerHTML +="Expression is:  " + expression;}
-	else if(isNaN(tot)) return 1;  //返回一个很小的fitness
+	if(TARGET - tot == 0) {
+		document.getElementById("info").innerHTML +="Expression is:  " + expression;
+		document.getElementById("info").innerHTML += ".  Generation: " + Generation;
+		//alert(Generation);
+		exit();
+	}
+	else if(isNaN(tot)) return 0;  //返回一个很小的fitness
 	else return 1.0/Math.abs(TARGET - tot);  //表达式的值越接近，fitness值越大！！
 }
 
 //alert("fitness is:  " + fitness("9+9/3"));
 
-//根据概率选出两条基因，输入genes库（数组），输出两条genes（数组）
+//根据概率选出一条基因，并将该基因从库中删除！输入genes库（数组），输出genes（string）
 /****算法：把数组元素值求和（记为S），那么生成一个0-S之间的均匀分布随机数P，然后从头到尾遍历数组，并在每步迭代中用P与数组当前位置i的累加值Si进行比较，就可以找到所需的数组元素。*****/
 function selectMember(genes){
-	var length = genes.length;
-	
-	var fitnesses = new Array(length);  //用来保存fitnesses	的数组
+	var length = genes.length;	
+	var fitnesses = new Array(length);//用来保存fitnesses	的数组
 	for (var i=0; i<length; i++) {
-		fitnesses[i] = fitness(decode(genes[i]));  //计算第i条gene的fitness
+		fitnesses[i] = fitness(decode(genes[i])); //计算第i条gene的fitness
 	}
-	
 	var tot = 0.0; //fitnesses 总和
 	for (var j=0; j<length; j++) {
 		tot += fitnesses[j];
 	}
 	
-	var place1 = Math.random() * tot;
-	var place2 = Math.random() * tot;
+	var place = Math.random() * tot;
 	var tot2 = 0.0;
-	var genePlace1 = -1, genePlace2 = -1;
+	var genePlace = 0;
 	for (var k = 0; k < length; k++) {
 		tot2 += fitnesses[k];
-		if(tot2 >= place1 && genePlace1 == -1) genePlace1 = k;//alert("find genePlace1: " + genePlace1);}
-		if(tot2 >= place2 && genePlace2 == -1) genePlace2 = k;//alert("find genePlace2: " + genePlace2);}
-		if(genePlace1 != -1 && genePlace2 != -1) {
-			//alert("即将break");
+		if(tot2 >= place ) {
+			genePlace = k;
 			break;
-		}
-		//alert("The genePlace1 is:  " + k);
-		
+		}	
 	}
-	//alert("genePlaces is:  " + genePlace1 + ", " + genePlace2);
-	return [genes.slice(genePlace1,genePlace1+1),genes.slice(genePlace2,genePlace2+1)];
+	return genes.splice(genePlace,1);
 }
 
-/* 
-var genes = initGenePools();
-alert(selectMember(genes));
- */
+ 
+/* initGenePools();
+alert(selectMember(genes)); */
+
 
 //输入两条基因，对其进行crossover
-function crossover(twoGenes){
-	var length = twoGenes[0].length;
+function crossover(gene1, gene2){
+	var length = gene1.length;
 	if(Math.random() <= CROSSRATE) {
 		var node = Math.round(Math.random() * (length -1));
-		var temp = twoGenes[0].slice(node);
-		//twoGenes[1].splice(node,length - node - 1,temp);
-		twoGenes[0] = twoGenes[0].slice(0,node) + twoGenes[1].slice(node);
-		twoGenes[1] = twoGenes[1].slice(0,node) + temp;
+		var temp = gene1.slice(node+1);
+		//gene2.splice(node,length - node - 1,temp);
+		gene1 = gene1.slice(0,node+1) + gene2.slice(node+1);
+		gene2 = gene2.slice(0,node+1) + temp;
 	}
 	//alert(twoGenes);
-	return twoGenes;
+	return [gene1, gene2];
 }
 
+/* a = ["1111111111","0000000000"];
+a = crossover(a[0],a[1]);
+alert(a); */
 /* var a = ["00000000","11111111"];
 for(var i=0; i<10; i++){
 	a = crossover(a);
